079642e89fe9e37699d45ff046a4af56a125edda
Carlos Eduardo de Borba Machado <carlosebmachado@outlook.com>
2022-05-01 00:09:18 -0300

    save

--- MessengerUC.Designer.cs
+++ MessengerUC.Designer.cs
@@ -29,21 +29,30 @@
         private void InitializeComponent()
         {
             this.btnSend = new System.Windows.Forms.Button();
-            this.txtMessage = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.txtMessage = new System.Windows.Forms.TextBox();
             this.rtbMessageField = new System.Windows.Forms.RichTextBox();
+            this.cbUsers = new System.Windows.Forms.ComboBox();
             this.menuStrip = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiOnline = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiOffiline = new System.Windows.Forms.ToolStripMenuItem();
             this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tsslName = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslStatus = new System.Windows.Forms.ToolStripStatusLabel();
             this.tableLayoutPanel.SuspendLayout();
             this.menuStrip.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(722, 400);
+            this.btnSend.Location = new System.Drawing.Point(722, 377);
             this.btnSend.Name = "btnSend";
             this.btnSend.Size = new System.Drawing.Size(75, 23);
             this.btnSend.TabIndex = 1;
@@ -51,51 +60,64 @@
             this.btnSend.UseVisualStyleBackColor = true;
             this.btnSend.Click += new System.EventHandler(this.SendMessageClick);
             // 
-            // txtMessage
-            // 
-            this.txtMessage.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtMessage.Location = new System.Drawing.Point(3, 400);
-            this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(713, 23);
-            this.txtMessage.TabIndex = 0;
-            this.txtMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextFieldMessageKeyDown);
-            // 
             // tableLayoutPanel
             // 
-            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnCount = 3;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.tableLayoutPanel.Controls.Add(this.txtMessage, 1, 1);
             this.tableLayoutPanel.Controls.Add(this.rtbMessageField, 0, 0);
-            this.tableLayoutPanel.Controls.Add(this.btnSend, 1, 1);
-            this.tableLayoutPanel.Controls.Add(this.txtMessage, 0, 1);
+            this.tableLayoutPanel.Controls.Add(this.btnSend, 2, 1);
+            this.tableLayoutPanel.Controls.Add(this.cbUsers, 0, 1);
             this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel.Location = new System.Drawing.Point(0, 24);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.Padding = new System.Windows.Forms.Padding(0, 0, 0, 23);
             this.tableLayoutPanel.RowCount = 2;
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
             this.tableLayoutPanel.Size = new System.Drawing.Size(800, 426);
             this.tableLayoutPanel.TabIndex = 7;
             // 
+            // txtMessage
+            // 
+            this.txtMessage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtMessage.Location = new System.Drawing.Point(103, 377);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(613, 23);
+            this.txtMessage.TabIndex = 0;
+            this.txtMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextFieldMessageKeyDown);
+            // 
             // rtbMessageField
             // 
             this.rtbMessageField.BackColor = System.Drawing.Color.White;
-            this.tableLayoutPanel.SetColumnSpan(this.rtbMessageField, 2);
+            this.tableLayoutPanel.SetColumnSpan(this.rtbMessageField, 3);
             this.rtbMessageField.Dock = System.Windows.Forms.DockStyle.Fill;
             this.rtbMessageField.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.rtbMessageField.Location = new System.Drawing.Point(3, 3);
             this.rtbMessageField.Name = "rtbMessageField";
             this.rtbMessageField.ReadOnly = true;
             this.rtbMessageField.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.rtbMessageField.Size = new System.Drawing.Size(794, 391);
+            this.rtbMessageField.Size = new System.Drawing.Size(794, 368);
             this.rtbMessageField.TabIndex = 2;
             this.rtbMessageField.TabStop = false;
             this.rtbMessageField.Text = "";
+            // 
+            // cbUsers
+            // 
+            this.cbUsers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbUsers.FormattingEnabled = true;
+            this.cbUsers.Location = new System.Drawing.Point(3, 377);
+            this.cbUsers.Name = "cbUsers";
+            this.cbUsers.Size = new System.Drawing.Size(94, 23);
+            this.cbUsers.TabIndex = 2;
             // 
             // menuStrip
             // 
             this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
+            this.statusToolStripMenuItem,
             this.aboutToolStripMenuItem});
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
@@ -127,6 +149,31 @@
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitClick);
             // 
+            // statusToolStripMenuItem
+            // 
+            this.statusToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiOnline,
+            this.tsmiOffiline});
+            this.statusToolStripMenuItem.Name = "statusToolStripMenuItem";
+            this.statusToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.statusToolStripMenuItem.Text = "Status";
+            // 
+            // tsmiOnline
+            // 
+            this.tsmiOnline.CheckOnClick = true;
+            this.tsmiOnline.Name = "tsmiOnline";
+            this.tsmiOnline.Size = new System.Drawing.Size(110, 22);
+            this.tsmiOnline.Text = "Online";
+            this.tsmiOnline.Click += new System.EventHandler(this.SetStatusOnlineClick);
+            // 
+            // tsmiOffiline
+            // 
+            this.tsmiOffiline.CheckOnClick = true;
+            this.tsmiOffiline.Name = "tsmiOffiline";
+            this.tsmiOffiline.Size = new System.Drawing.Size(110, 22);
+            this.tsmiOffiline.Text = "Offline";
+            this.tsmiOffiline.Click += new System.EventHandler(this.SetStatusOffilineClick);
+            // 
             // aboutToolStripMenuItem
             // 
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
@@ -134,11 +181,42 @@
             this.aboutToolStripMenuItem.Text = "About";
             this.aboutToolStripMenuItem.Click += new System.EventHandler(this.AboutClick);
             // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslName,
+            this.toolStripStatusLabel1,
+            this.tsslStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip";
+            // 
+            // tsslName
+            // 
+            this.tsslName.Name = "tsslName";
+            this.tsslName.Size = new System.Drawing.Size(97, 17);
+            this.tsslName.Text = "Name: username";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(10, 17);
+            this.toolStripStatusLabel1.Text = "|";
+            // 
+            // tsslStatus
+            // 
+            this.tsslStatus.Name = "tsslStatus";
+            this.tsslStatus.Size = new System.Drawing.Size(84, 17);
+            this.tsslStatus.Text = "Status: Offiline";
+            // 
             // MessengerUC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.tableLayoutPanel);
             this.Controls.Add(this.menuStrip);
             this.Name = "MessengerUC";
@@ -147,6 +225,8 @@
             this.tableLayoutPanel.PerformLayout();
             this.menuStrip.ResumeLayout(false);
             this.menuStrip.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,7 +235,6 @@
         #endregion
 
         private Button btnSend;
-        private TextBox txtMessage;
         private TableLayoutPanel tableLayoutPanel;
         private RichTextBox rtbMessageField;
         private MenuStrip menuStrip;
@@ -163,5 +242,14 @@
         private ToolStripMenuItem exitToolStripMenuItem;
         private ToolStripMenuItem aboutToolStripMenuItem;
         private ToolStripMenuItem logoutToolStripMenuItem;
+        private TextBox txtMessage;
+        private ComboBox cbUsers;
+        private ToolStripMenuItem statusToolStripMenuItem;
+        private ToolStripMenuItem tsmiOnline;
+        private ToolStripMenuItem tsmiOffiline;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel tsslName;
+        private ToolStripStatusLabel tsslStatus;
+        private ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
--- MessengerUC.cs
+++ MessengerUC.cs
@@ -6,8 +6,15 @@ namespace Messenger
     public partial class MessengerUC : UserControl
     {
         private ArtalkXmppClient client;
+        private Roster roster;
+        private string name = null;
 
         private readonly Delegate exit;
+
+        private string[] users = {
+            "admin@desktop-22frc2k.dlink",
+            "carlos@desktop-22frc2k.dlink"
+        };
 
         public MessengerUC(ArtalkXmppClient client, Delegate exit)
         {
@@ -16,10 +23,18 @@ namespace Messenger
             this.exit = exit;
             SetClient(client);
 
-            rtbMessageField.Text = "Bem vindo!\n\n";
             Focus();
             Select();
             txtMessage.Select();
+
+            foreach (var user in users)
+            {
+                cbUsers.Items.Add(user);
+            }
+            cbUsers.SelectedItem = users[0];
+
+            tsmiOnline.Checked = false;
+            tsmiOffiline.Checked = true;
         }
 
         public void SetClient(ArtalkXmppClient client)
@@ -27,6 +42,35 @@ namespace Messenger
             if (client == null) return;
             this.client = client;
             this.client.Message += OnNewMessage;
+            this.client.StatusChanged += Client_StatusChanged;
+            SetName();
+        }
+
+        private void Client_StatusChanged(object? sender, StatusEventArgs e)
+        {
+            if (e.Status.Availability == Availability.Online)
+            {
+                tsslStatus.Text = "Status: Online";
+                tsmiOffiline.Checked = false;
+                tsmiOnline.Checked = true;
+            }
+            else
+            {
+                tsslStatus.Text = "Status: Offline";
+                tsmiOffiline.Checked = true;
+                tsmiOnline.Checked = false;
+            }
+        }
+
+        private void SetName()
+        {
+            if (name == null)
+            {
+                name = client.Username;
+                name = char.ToUpper(name[0]) + name[1..];
+
+                tsslName.Text = "Name: " + name;
+            }
         }
 
         private void SendMessageClick(object sender, EventArgs e)
@@ -38,16 +82,31 @@ namespace Messenger
         {
             if (txtMessage.Text == "") return;
             if (!client.Connected) return;
-            //MessageBox.Show(client.Jid.ToString());
-            client.SendMessage("chat@conference.desktop-22frc2k.dlink", client.Jid.ToString().Split("@")[0] + ":" + txtMessage.Text);
+            SetName();
+            client.SendMessage(cbUsers.Text, name + ":" + txtMessage.Text);
+            AddMessage(name, txtMessage.Text);
             txtMessage.Text = "";
+        }
+
+        private void SetStatus(Availability status)
+        {
+            if (client.Connected)
+            {
+                try
+                {
+                    client.SetStatus(status);
+                }
+                catch(Exception e)
+                {
+
+                }
+            }
         }
 
         void OnNewMessage(object? sender, MessageEventArgs e)
         {
             var body = e.Message.Body.Trim();
             var user = body.Split(':')[0];
-            user = char.ToUpper(user[0]) + user[1..];
             var message = body[(user.Length + 1)..];
             AddMessage(user, message);
         }
@@ -80,6 +139,20 @@ namespace Messenger
             {
                 SendMessage();
             }
+        }
+
+        private void SetStatusOnlineClick(object sender, EventArgs e)
+        {
+            SetStatus(Availability.Online);
+            tsmiOffiline.Checked = true;
+            tsmiOnline.Checked = false;
+        }
+
+        private void SetStatusOffilineClick(object sender, EventArgs e)
+        {
+            SetStatus(Availability.Offline);
+            tsmiOffiline.Checked = false;
+            tsmiOnline.Checked = true;
         }
     }
 }
